31893da5a3c71e669f3cf19b0f5d8f5fb1bd7c1f
Chaitnya Shinde <roshanshinde2694@gmail.com>
2026-08-02 17:59:17 +0530

    Delete BookShoppingCartMvcUI/GlobalUsings.cs

--- BookShoppingCartMvcUI/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using BookShoppingCartMvcUI.Data;
-global using BookShoppingCartMvcUI.Models;
-global using BookShoppingCartMvcUI.Models.DTOs;
-global using BookShoppingCartMvcUI.Repositories;
-global using BookShoppingCartMvcUI.Constants;
